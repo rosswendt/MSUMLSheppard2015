c0ded9091bc0f51565c13341051abfc533383500
Lukas Keller <lukas.keller@msu.montana.edu>
2015-10-10 14:57:54 -0600

    Update FunctionToApproximate.java

--- src/DataPack/FunctionToApproximate.java
+++ src/DataPack/FunctionToApproximate.java
@@ -7,8 +7,17 @@ package DataPack;
 public class FunctionToApproximate {
     int outputYVal;
     
-    public FunctionToApproximate() {
-        
+    public static double[] FunctionToApproximate(double[] x) {      //hyperbolic tangent
+    double [] y;
+    y = new double[x.length];
+       
+    for(int i=0; i<x.length; i++){
+    double a = Math.exp( x[i] );
+    double b = Math.exp( 0 - (x[i]));
+    y[i]=((a-b)/(a+b));
+}
+    
+    return y;
     }
     
     public double computeRosenBrockOutVal(double[] x) {
