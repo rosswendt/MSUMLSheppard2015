23da38477302774ee65d22ea3ba317a37d8d953d
Lukas Keller <lukas.keller@msu.montana.edu>
2015-10-10 11:28:45 -0600

    Update FunctionToApproximate.java

--- src/DataPack/FunctionToApproximate.java
+++ src/DataPack/FunctionToApproximate.java
@@ -11,9 +11,12 @@ public class FunctionToApproximate {
         
     }
     
-    int computeRosenBrockOutVal() {
+    public double computeRosenBrockOutVal(double[] x) {
         // f(x) = sigma 100*(xi+1 - xi^2)^2 + (1-xi)^2      <--wiki
         //f(x,y) = (a-z)^2 + b(y-x^2)^2                     <--wiki
+        for(int i=0; i<x.length; i++){
+        outputYVal+= ((1-x[i])*(1-x[i])) + 100*((x[i+1]-(x[i]*x[i]))*(x[i+1]-(x[i]*x[i]))); 
+    }
         
         return outputYVal;
     }
