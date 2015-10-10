b0e90ebfff7d90d2e024285ad51e60a2dfd29dbe
Lukas Keller <lukas.keller@msu.montana.edu>
2015-10-10 03:03:52 -0600

    Update Point.java
    Skeleton code was rather unclear in which direction stuff was going and I was too afraid to change much in the other classes.

--- src/DataPack/Point.java
+++ src/DataPack/Point.java
@@ -2,27 +2,40 @@ package DataPack;
 
 /**
  *
- * @author Ross Wendt
+ * @author Ross Wendt, Lukas Keller
  */
 public class Point {
     int generatedXVal;
+    
     
     public Point(int startGeneratedXVal) {
         generatedXVal = startGeneratedXVal;
     }
     
-    public boolean isTrainingSet() {
+    public boolean isTrainingSet(int x) {
         boolean b = false;
+        if(x == 1){
+            b = true;
+        }
         return b;
     }
     
-    public boolean isTestSet() {
+    public boolean isTestSet(int x) {
         boolean b = false;
+        if(x == 1){
+            b = true;
+        }
         return b;
     }
     
-    public Point generatePoint() {
-        Point p = null;
+    public static Point generatePoint(int x) {
+        Point p = new Point(x);
         return p;
     }
+    
+      public int generatedXVal(int generatedXVal) {
+        this.generatedXVal = generatedXVal;
+        return generatedXVal;
+    }
+
 }
