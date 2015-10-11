028bfdf1de1589fffacb90d6bc9c6c170e1b2295
angustomlinson <thekingsbondslave@gmail.com>
2015-10-10 18:47:14 -0600

    Update Driver.java
    Ignore this; this is my rudimentary driver for testing the neural network code.

--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -4,29 +4,59 @@ import NeuralNet.*;
 
 /**
  *
- * @author Ross Wendt
+ * @author Angus Tomlinson
  */
-public class Driver { 
-    
-    static Point[] dataSet;
-    NeuralNet driverNet = new NeuralNet(0,0,0,0);
-    
-    public static void main(String Args[]) {
-        dataSet = generateData();
-        run(dataSet);
-    }
-    
-    
-    public static Point[] generateData() {
-        Point[] P = null;
-        return P;
-    }
-    
-    public static void run(Point[] P) {
-        //run code goes here
-    }
-    
-    //perhaps various methods for printing different things
+public class Driver {
 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double[][] matrixA = {{1, 2, 3}};
+        double[][] matrixB = {{0}};
+        int[] hiddenLayers = {2, 1, 4, 5};
+
+        NeuralNet neuralNet = new NeuralNet(matrixA, matrixB, hiddenLayers);
+
+        Matrix c;
+
+        for (int k = 0; k < neuralNet.getWeightMatrixes().length; k++) {
+            c = neuralNet.getWeightMatrixes()[k];
+            for (int i = 0; i < c.getRows(); i++) {
+                for (int j = 0; j < c.getColumns(); j++) {
+                    System.out.print(c.getMatrixValues()[i][j] + " ");
+                    if (c.getMatrixValues()[i][j] >= 10000) {
+                    } else if (c.getMatrixValues()[i][j] >= 1000) {
+                        System.out.print(" ");
+                    } else if (c.getMatrixValues()[i][j] >= 100) {
+                        System.out.print("  ");
+                    } else if (c.getMatrixValues()[i][j] >= 10) {
+                        System.out.print("   ");
+                    } else {
+                        System.out.print("    ");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        neuralNet.forwardPropagation();
+        c = neuralNet.getOutputMatrix();
+        for (int i = 0; i < c.getRows(); i++) {
+            for (int j = 0; j < c.getColumns(); j++) {
+                System.out.print(c.getMatrixValues()[i][j] + " ");
+                if (c.getMatrixValues()[i][j] >= 10000) {
+                } else if (c.getMatrixValues()[i][j] >= 1000) {
+                    System.out.print(" ");
+                } else if (c.getMatrixValues()[i][j] >= 100) {
+                    System.out.print("  ");
+                } else if (c.getMatrixValues()[i][j] >= 10) {
+                    System.out.print("   ");
+                } else {
+                    System.out.print("    ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
-
