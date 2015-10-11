e0fbd0f7283becfb1af1d67f846240f7fcd1b940
angustomlinson <thekingsbondslave@gmail.com>
2015-10-10 18:36:20 -0600

    Updated NeuralNet.java
    Initializes neural network; forward propagation available; back propagation will be up later tonight.

--- src/NeuralNet/NeuralNet.java
+++ src/NeuralNet/NeuralNet.java
@@ -1,25 +1,62 @@
 package NeuralNet;
-import NeuralNetHelper.*;
-
 
 /**
  *
- * @author Ross Wendt
+ * @author Angus Tomlinson
  */
 public class NeuralNet {
-    
-    public NeuralNet(int startNumInputs, int startNumOutputs, int startNumHiddenLayers, int startUnitsPerLayer ) {
-        
+
+    private Matrix inputMatrix;
+    private Matrix outputMatrix;
+    private Matrix[] weightMatrixes;
+    private Matrix[] sMatrixes;
+    private Matrix[] zMatrixes;
+    private final MatrixOperations matrixOperations = new MatrixOperations();
+
+    public NeuralNet(double[][] input, double[][] output, int[] hiddenLayers) {
+        inputMatrix = new Matrix(input);
+        outputMatrix = new Matrix(output);
+        weightMatrixes = new Matrix[hiddenLayers.length + 1];
+        if (weightMatrixes.length == 1) {
+            weightMatrixes[0] = new Matrix(new double[inputMatrix.getColumns()][outputMatrix.getColumns()]);
+        } else {
+            weightMatrixes[0] = new Matrix(new double[inputMatrix.getColumns()][hiddenLayers[0]]);
+            for (int i = 1; i < hiddenLayers.length; i++) {
+                weightMatrixes[i] = new Matrix(new double[hiddenLayers[i - 1]][hiddenLayers[i]]);
+            }
+            weightMatrixes[weightMatrixes.length - 1] = new Matrix(new double[hiddenLayers[hiddenLayers.length - 1]][outputMatrix.getColumns()]);
+        }
+        setWeights();
+        sMatrixes = new Matrix[hiddenLayers.length];
+        zMatrixes = new Matrix[hiddenLayers.length];
     }
-    
-    public Matrix backProp() {
-        Matrix M = new Matrix(0,0);
-        return M;
+
+    public void setWeights() {
+        for (int i = 0; i < weightMatrixes.length; i++) {
+            weightMatrixes[i] = matrixOperations.randomlyInitializeMatrix(weightMatrixes[i], 2);
+        }
     }
-    
-    public Matrix feedForward() {
-        Matrix M = new Matrix(0,0);
-        return M;
+
+    public void forwardPropagation() {
+        sMatrixes[0] = matrixOperations.multiplyMatrixes(inputMatrix, weightMatrixes[0]);
+        zMatrixes[0] = matrixOperations.scalarMultiply(2, sMatrixes[0]);
+        for (int i = 1; i < sMatrixes.length; i++) {
+            sMatrixes[i] = matrixOperations.multiplyMatrixes(zMatrixes[i - 1], weightMatrixes[i]);
+            zMatrixes[i] = matrixOperations.scalarMultiply(2, sMatrixes[i]);
+        }
+        outputMatrix = matrixOperations.multiplyMatrixes(zMatrixes[zMatrixes.length - 1],
+                weightMatrixes[weightMatrixes.length - 1]);
     }
-    
+
+    public Matrix getInputMatrix() {
+        return inputMatrix;
+    }
+
+    public Matrix getOutputMatrix() {
+        return outputMatrix;
+    }
+
+    public Matrix[] getWeightMatrixes() {
+        return weightMatrixes;
+    }
 }
