0b75b5ad4f5b04b75ff4992386803e974f883a32
Lukas Keller <lukas.keller@msu.montana.edu>
2015-10-10 18:07:05 -0600

    Update FunctionToApproximate.java
    added hyperbolic tangent derivative function that takes an array as input

--- src/DataPack/FunctionToApproximate.java
+++ src/DataPack/FunctionToApproximate.java
@@ -20,6 +20,19 @@ public class FunctionToApproximate {
     return y;
     }
     
+    public static double[] tanhDerivative(double[] x){              //takes an array and applies the derivative of hyperbolic tangent to it. that deriv is 1-tanh^2
+        double[] y = new double[x.length];
+        double tan = 0;
+        for(int i=0; i<x.length; i++){
+        double a = Math.exp( x[i] );
+        double b = Math.exp( 0 - (x[i]));
+        tan =((a-b)/(a+b));
+        y[i] = 1-(tan * tan);
+        
+    }
+        return y;
+    }
+    
     public double computeRosenBrockOutVal(double[] x) {
         // f(x) = sigma 100*(xi+1 - xi^2)^2 + (1-xi)^2      <--wiki
         //f(x,y) = (a-z)^2 + b(y-x^2)^2                     <--wiki
