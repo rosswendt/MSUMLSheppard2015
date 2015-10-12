b0f8f5e81f9c2cb64bcbe6f0f971e0d382247898
angustomlinson <thekingsbondslave@gmail.com>
2015-10-12 01:25:05 -0600

    Update Driver.java

--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -12,16 +12,37 @@ public class Driver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double[][] matrixA = {{1, 2, 3}};
-        double[][] matrixB = {{0}};
-        int[] hiddenLayers = {2, 1, 4, 5};
+        double[][] matrixA = {{1, 2}};
+        double[][] matrixB = {{1}};
+        int[] hiddenLayers = {3};
 
         NeuralNet neuralNet = new NeuralNet(matrixA, matrixB, hiddenLayers);
 
-        Matrix c;
+        neuralNet.forwardPropagation();
+        Matrix c = neuralNet.getError();
 
-        for (int k = 0; k < neuralNet.getWeightMatrixes().length; k++) {
-            c = neuralNet.getWeightMatrixes()[k];
+        for (int i = 0; i < c.getRows(); i++) {
+            for (int j = 0; j < c.getColumns(); j++) {
+                System.out.print(c.getMatrixValues()[i][j] + " ");
+                if (c.getMatrixValues()[i][j] >= 10000) {
+                } else if (c.getMatrixValues()[i][j] >= 1000) {
+                    System.out.print(" ");
+                } else if (c.getMatrixValues()[i][j] >= 100) {
+                    System.out.print("  ");
+                } else if (c.getMatrixValues()[i][j] >= 10) {
+                    System.out.print("   ");
+                } else {
+                    System.out.print("    ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int k = 0; k < 1000; k++) {
+            neuralNet.backPropagation();
+            neuralNet.forwardPropagation();
+            c = neuralNet.getOutputMatrix();
+            
             for (int i = 0; i < c.getRows(); i++) {
                 for (int j = 0; j < c.getColumns(); j++) {
                     System.out.print(c.getMatrixValues()[i][j] + " ");
@@ -38,23 +59,41 @@ public class Driver {
                 }
                 System.out.println();
             }
-            System.out.println();
-        }
-        neuralNet.forwardPropagation();
-        c = neuralNet.getOutputMatrix();
-        for (int i = 0; i < c.getRows(); i++) {
-            for (int j = 0; j < c.getColumns(); j++) {
-                System.out.print(c.getMatrixValues()[i][j] + " ");
-                if (c.getMatrixValues()[i][j] >= 10000) {
-                } else if (c.getMatrixValues()[i][j] >= 1000) {
-                    System.out.print(" ");
-                } else if (c.getMatrixValues()[i][j] >= 100) {
-                    System.out.print("  ");
-                } else if (c.getMatrixValues()[i][j] >= 10) {
-                    System.out.print("   ");
-                } else {
-                    System.out.print("    ");
+            c = neuralNet.getTargetOutputMatrix();
+            
+            for (int i = 0; i < c.getRows(); i++) {
+                for (int j = 0; j < c.getColumns(); j++) {
+                    System.out.print(c.getMatrixValues()[i][j] + " ");
+                    if (c.getMatrixValues()[i][j] >= 10000) {
+                    } else if (c.getMatrixValues()[i][j] >= 1000) {
+                        System.out.print(" ");
+                    } else if (c.getMatrixValues()[i][j] >= 100) {
+                        System.out.print("  ");
+                    } else if (c.getMatrixValues()[i][j] >= 10) {
+                        System.out.print("   ");
+                    } else {
+                        System.out.print("    ");
+                    }
                 }
+                System.out.println();
+            }
+            c = neuralNet.getError();
+            System.out.println("Error:");
+            for (int i = 0; i < c.getRows(); i++) {
+                for (int j = 0; j < c.getColumns(); j++) {
+                    System.out.print(c.getMatrixValues()[i][j] + " ");
+                    if (c.getMatrixValues()[i][j] >= 10000) {
+                    } else if (c.getMatrixValues()[i][j] >= 1000) {
+                        System.out.print(" ");
+                    } else if (c.getMatrixValues()[i][j] >= 100) {
+                        System.out.print("  ");
+                    } else if (c.getMatrixValues()[i][j] >= 10) {
+                        System.out.print("   ");
+                    } else {
+                        System.out.print("    ");
+                    }
+                }
+                System.out.println();
             }
             System.out.println();
         }
