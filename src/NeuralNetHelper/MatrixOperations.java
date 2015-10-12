f864c2adfe03b588a58a1ed73545a68a0b6be7af
angustomlinson <thekingsbondslave@gmail.com>
2015-10-11 20:18:01 -0600

    Update MatrixOperations.java

--- src/NeuralNetHelper/MatrixOperations.java
+++ src/NeuralNetHelper/MatrixOperations.java
@@ -1,38 +1,136 @@
 package NeuralNetHelper;
 
+
+import java.util.Random;
+
 /**
  *
  * @author Angus Tomlinson
  */
-public class Matrix {
-    private final int rows;
-    private final int columns;
-    private final double[][] matrixValues;
+public class MatrixOperations {
+    private static final int r = 1;
+    private static final int c = 2;
     
-    /**
-     *
-     * @param initiapackage mlproject2;
-
-/**lMatrixValues
-     */
-    public Matrix(double[][] initialMatrixValues){
-        rows = initialMatrixValues.length;
-        columns = initialMatrixValues[0].length;
-        matrixValues = new double[rows][columns];
-        for(int i = 0; i < matrixValues.length; i++){
-            System.arraycopy(initialMatrixValues[i], 0, matrixValues[i], 0, matrixValues[0].length);
+    public static Matrix initializeBiasMatrix(double theta, Matrix a){
+        for(int i = 0; i < a.getRows(); i++){
+            for(int j = 0; j < a.getColumns(); j++){
+                a.getMatrixValues()[i][j] = theta;
+            }
         }
+        return a;
     }
     
-    public int getRows(){
-        return rows;
+    public static Matrix randomlyInitializeMatrix(Matrix a){
+        Random randomGenerator = new Random();
+        for(int i = 0; i < a.getRows(); i++){
+            for(int j = 0; j < a.getColumns(); j++){
+                a.getMatrixValues()[i][j] = randomGenerator.nextDouble();
+            }
+        }
+        return a;
     }
     
-    public int getColumns(){
-        return columns;
+    public static Matrix addMatrixes(Matrix a, Matrix b){
+        double[][] zeroMatrix = new double[a.getRows()][b.getColumns()];
+        Matrix resultMatrix = new Matrix(zeroMatrix);
+        for(int i = 0; i < resultMatrix.getRows(); i++){
+            for(int j = 0; j < resultMatrix.getColumns(); j++){
+                resultMatrix.getMatrixValues()[i][j] = a.getMatrixValues()[i][j] + b.getMatrixValues()[i][j];
+            }
+        }
+        return resultMatrix;
     }
     
-    public double[][] getMatrixValues(){
-        return matrixValues;
+    public static Matrix subtractMatrixes(Matrix a, Matrix b){
+        double[][] zeroMatrix = new double[a.getRows()][b.getColumns()];
+        Matrix resultMatrix = new Matrix(zeroMatrix);
+        for(int i = 0; i < resultMatrix.getRows(); i++){
+            for(int j = 0; j < resultMatrix.getColumns(); j++){
+                resultMatrix.getMatrixValues()[i][j] = a.getMatrixValues()[i][j] - b.getMatrixValues()[i][j];
+            }
+        }
+        return resultMatrix;
+    }
+    
+    public static Matrix scalarMultiply(double scalar, Matrix a){
+        for(int i = 0; i < a.getRows(); i++){
+            for(int j = 0; j < a.getColumns(); j++){
+                a.getMatrixValues()[i][j] = a.getMatrixValues()[i][j] * scalar;
+            }
+        }
+        return a;
+    }
+    
+    public static Matrix transpose(Matrix a){
+        double[][] zeroMatrix = new double[a.getRows()][a.getColumns()];
+        Matrix resultMatrix = new Matrix(zeroMatrix);
+        for(int i = 0; i < a.getRows(); i++){
+            for(int j = 0; j < a.getColumns(); j++){
+                resultMatrix.getMatrixValues()[j][i] = a.getMatrixValues()[i][j];
+            }
+        }
+        return resultMatrix;
+    }
+    
+    public static Matrix tanh(Matrix a){
+        for(int i = 0; i < a.getRows(); i++){
+            for(int j = 0; j < a.getColumns(); j++){
+                a.getMatrixValues()[i][j] = Functions.tanh(a.getMatrixValues()[i][j]);
+            }
+        }
+        return a;
+    }
+    
+    public static Matrix derivativeTanh(Matrix a){
+        for(int i = 0; i < a.getRows(); i++){
+            for(int j = 0; j < a.getColumns(); j++){
+                a.getMatrixValues()[i][j] = Functions.tanhDerivative(a.getMatrixValues()[i][j]);
+            }
+        }
+        return a;
+    }
+    
+    public static Matrix gaussian(Matrix a){
+        for(int i = 0; i < a.getRows(); i++){
+            for(int j = 0; j < a.getColumns(); j++){
+                a.getMatrixValues()[i][j] = Functions.gaussian(r, c, a.getMatrixValues()[i][j]);
+            }
+        }
+        return a;
+    }
+    
+    public static Matrix derivativeGaussian(Matrix a){
+        for(int i = 0; i < a.getRows(); i++){
+            for(int j = 0; j < a.getColumns(); j++){
+                a.getMatrixValues()[i][j] = Functions.gaussianDerivative(r, c, a.getMatrixValues()[i][j]);
+            }
+        }
+        return a;
+    }
+    
+    public static Matrix multiplyMatrixes(Matrix a, Matrix b){
+        double[][] zeroMatrix = new double[a.getRows()][b.getColumns()];
+        Matrix resultMatrix = new Matrix(zeroMatrix);
+        for(int j = 0; j < b.getColumns(); j++){
+            for(int k = 0; k < a.getRows(); k++){
+                double result = 0;
+                for(int i = 0; i < b.getRows(); i++){
+                    result += (a.getMatrixValues()[k][i] * b.getMatrixValues()[i][j]);
+                }
+                resultMatrix.getMatrixValues()[k][j] = result;
+            }
+        }
+        return resultMatrix;
+    }
+    
+    public static Matrix hadamardProduct(Matrix a, Matrix b){
+        double[][] zeroMatrix = new double[a.getRows()][a.getColumns()];
+        Matrix resultMatrix = new Matrix(zeroMatrix);
+        for(int i = 0; i < a.getRows(); i++){
+            for(int j = 0; j < a.getColumns(); j++){
+                resultMatrix.getMatrixValues()[i][j] = a.getMatrixValues()[i][j] * b.getMatrixValues()[i][j];
+            }
+        }
+        return resultMatrix;
     }
 }
