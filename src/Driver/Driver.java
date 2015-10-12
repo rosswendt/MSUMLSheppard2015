018a2b629efb8817136899ad046d0b3a3515e583
Ross <rosswendt@users.noreply.github.com>
2015-10-11 20:35:48 -0600

    Update Driver.java

--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -1,6 +1,7 @@
 package Driver;
 import DataPack.*;
 import NeuralNet.*;
+import NeuralNetHelper.Matrix;
 
 /**
  *
@@ -12,8 +13,13 @@ public class Driver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double[][] matrixA = {{1, 2, 3}};
-        double[][] matrixB = {{0}};
+        double stepsize = .01; //value for how little or how large we want to step through the function
+        int dimension = 5; //n per vector (as mentioned in Sheppard's pdf for proj 2)
+        double startVal = -1; //starting value on the domain we want to look at
+        double finVal = 1; //stopping value 
+        
+        double[][] matrixA = generateXVal(dimension, startVal, finVal, stepsize);
+        double[][] matrixB = computeRosenbrock(matrixA, stepsize);
         int[] hiddenLayers = {2, 1, 4, 5};
 
         NeuralNet neuralNet = new NeuralNet(matrixA, matrixB, hiddenLayers);
@@ -58,5 +64,47 @@ public class Driver {
             }
             System.out.println();
         }
+    }
+        
+    public static double[][] generateXVal(int dimension, double minval, double maxval, double step) {
+        double[][] list = new double[(int) Math.round(((maxval-minval)/step)/dimension)][dimension];
+        double temp = minval;
+        
+        for (int i = 0; i < list.length; i++ ) {
+            for (int j = 0; j < list[0].length; j++ ) {
+            list[i][j] = Math.round((temp / step)) * step; //This line sometimes corrects some double instability
+            temp += step;
+            System.out.print(list[i][j] + " ");
+            
+        }
+            System.out.println();
+
+        }
+         
+        return list;
+    }
+    
+    /* Generates the associated y values from the input x values using the 
+     *Rosenbrock function.
+     */
+    
+    public static double[][] computeRosenbrock(double[][] input, double step) {
+        
+        double[][] output = new double[input.length][1]; 
+        double outval = 0;
+        
+        for (int i = 0; i < input.length - 1; i++ ) {
+            for (int j = 0; j < input[0].length; j++ ) {
+            outval +=  step * ( (1 - input[i][j])*(1-input[i][j]) + 100 * ( ( input[i+1][j] - (input[i][j]*input[i][j]) )) * ( input[i+1][j] - (input[i][j]*input[i][j]) ) );
+            }
+            //System.out.println(outval);
+            output[i][0] = outval; // assign val to element in output
+            System.out.println();
+            System.out.println(output[i][0]);
+            outval = 0; //reset outval to 0 so the subsequent summations make sense (start with sum of 0)
+        }
+
+        return output;     
+        
     }
 }
