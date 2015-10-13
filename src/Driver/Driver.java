6808abd7d3328e1601d63afd9bd7fc115a6e7476
angustomlinson <thekingsbondslave@gmail.com>
2015-10-13 16:38:41 -0600

    Update Driver.java

--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -16,123 +16,107 @@ public class Driver {
     public static void main(String[] args) {
         double lowerBound = -5;
         double upperBound = 10;
-        int dataSetSize = 1000; // make sure this number is divisible by k
-        int k = 10; // number of folds
+        int dataSetSize = 4; // make sure this number is divisible by k
+        int k = 2; // number of folds
         int n = 2;
-        
+
         double[][] xDataSet = initializeXDataSet(dataSetSize, n, lowerBound, upperBound);
-        //double[][] yDataSet = initializeYDataSet(xDataSet);
-        
-        
-        
-        double[][] matrixA = {{4, 2}};//xDataSet[0];
-        System.out.println();
-        double[][] matrixB = {{20}};//yDataSet[0];
-        System.out.println();
-        int[] hiddenLayers = {1};
+        double[][] yDataSet = initializeYDataSet(xDataSet);
+
+        int[][] subsets = initializeSubsets(dataSetSize, k);
+
+        double[] matrixA = {0, 0};
+        double[] matrixB = {0};
+        int[] hiddenLayers = {3};
+        Matrix meanSquaredError;
 
         NeuralNet neuralNet = new NeuralNet(matrixA, matrixB, hiddenLayers);
-
-        neuralNet.forwardPropagation();
-        Matrix c = neuralNet.getError();
-
-        for (int i = 0; i < c.getRows(); i++) {
-            for (int j = 0; j < c.getColumns(); j++) {
-                System.out.print(c.getMatrixValues()[i][j] + " ");
-                if (c.getMatrixValues()[i][j] >= 10000) {
-                } else if (c.getMatrixValues()[i][j] >= 1000) {
-                    System.out.print(" ");
-                } else if (c.getMatrixValues()[i][j] >= 100) {
-                    System.out.print("  ");
-                } else if (c.getMatrixValues()[i][j] >= 10) {
-                    System.out.print("   ");
-                } else {
-                    System.out.print("    ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
-        for (int l = 0; l < 1000; l++) {
-            neuralNet.backPropagation();
-            neuralNet.forwardPropagation();
-            c = neuralNet.getOutputMatrix();
-            
-            for (int i = 0; i < c.getRows(); i++) {
-                for (int j = 0; j < c.getColumns(); j++) {
-                    System.out.print(c.getMatrixValues()[i][j] + " ");
-                    if (c.getMatrixValues()[i][j] >= 10000) {
-                    } else if (c.getMatrixValues()[i][j] >= 1000) {
-                        System.out.print(" ");
-                    } else if (c.getMatrixValues()[i][j] >= 100) {
-                        System.out.print("  ");
-                    } else if (c.getMatrixValues()[i][j] >= 10) {
-                        System.out.print("   ");
-                    } else {
-                        System.out.print("    ");
+        for (int j = 0; j < 100000000; j++) {
+            for (int testCounter = 0; testCounter < k; testCounter++) {
+                meanSquaredError = new Matrix(matrixB);
+                for (int trainingCounter = 0; trainingCounter < subsets.length; trainingCounter++) {
+                    if (trainingCounter != testCounter) {
+                        for (int i = 0; i < subsets[trainingCounter].length; i++) {
+                            neuralNet.setInputMatrix(xDataSet[subsets[trainingCounter][i]]);
+                            neuralNet.setTargetOutputMatrix(yDataSet[subsets[trainingCounter][i]]);
+                            neuralNet.forwardPropagation();
+                            meanSquaredError = MatrixOperations.addMatrices(meanSquaredError, neuralNet.getError());
+                            //System.out.println(neuralNet.getError().getMatrixValues()[0][0]);
+                            neuralNet.backPropagation();
+                        }
                     }
                 }
-                System.out.println();
-            }
-            c = neuralNet.getTargetOutputMatrix();
-            
-            for (int i = 0; i < c.getRows(); i++) {
-                for (int j = 0; j < c.getColumns(); j++) {
-                    System.out.print(c.getMatrixValues()[i][j] + " ");
-                    if (c.getMatrixValues()[i][j] >= 10000) {
-                    } else if (c.getMatrixValues()[i][j] >= 1000) {
-                        System.out.print(" ");
-                    } else if (c.getMatrixValues()[i][j] >= 100) {
-                        System.out.print("  ");
-                    } else if (c.getMatrixValues()[i][j] >= 10) {
-                        System.out.print("   ");
-                    } else {
-                        System.out.print("    ");
-                    }
+                System.out.println("Epoch" + j + ":");
+                System.out.println("MeanSquaredError for training set:" + meanSquaredError.getMatrixValues()[0][0] / ((k - 1)) * (dataSetSize / k));
+                meanSquaredError = new Matrix(matrixB);
+                for (int i = 0; i < subsets[testCounter].length; i++) {
+                    neuralNet.setInputMatrix(xDataSet[subsets[testCounter][i]]);
+                    neuralNet.setTargetOutputMatrix(yDataSet[subsets[testCounter][i]]);
+                    neuralNet.forwardPropagation();
+                    meanSquaredError = MatrixOperations.addMatrices(meanSquaredError, neuralNet.getError());
+                    //System.out.println(neuralNet.getError().getMatrixValues()[0][0]);
                 }
+                System.out.println("MeanSquaredError for test set:" + (meanSquaredError.getMatrixValues()[0][0] / (subsets[testCounter].length)));
                 System.out.println();
             }
-            c = neuralNet.getError();
-            System.out.println("Error:");
-            for (int i = 0; i < c.getRows(); i++) {
-                for (int j = 0; j < c.getColumns(); j++) {
-                    System.out.print(c.getMatrixValues()[i][j] + " ");
-                    if (c.getMatrixValues()[i][j] >= 10000) {
-                    } else if (c.getMatrixValues()[i][j] >= 1000) {
-                        System.out.print(" ");
-                    } else if (c.getMatrixValues()[i][j] >= 100) {
-                        System.out.print("  ");
-                    } else if (c.getMatrixValues()[i][j] >= 10) {
-                        System.out.print("   ");
-                    } else {
-                        System.out.print("    ");
-                    }
-                }
-                System.out.println();
-            }
-            System.out.println();
         }
     }
-    
-    public static double[][] initializeXDataSet(int samples, int n, double lowerBound, double upperBound){
+
+    public static double[][] initializeXDataSet(int samples, int n, double lowerBound, double upperBound) {
         Random rdm = new Random();
         double stepCounter = (upperBound - lowerBound) / (samples * n);
         double xValue = lowerBound;
-        double [][] xDataSet = new double[samples][n];
-        for(int i = 0; i < samples; i++){
-            for(int j = 0; j < n; j++){
+        double[][] xDataSet = new double[samples][n];
+        for (int i = 0; i < samples; i++) {
+            for (int j = 0; j < n; j++) {
                 xDataSet[i][j] = (rdm.nextDouble() * (upperBound - lowerBound)) + lowerBound;
             }
         }
-        
+
         return xDataSet;
     }
-    
-    public static double[][] initializeYDataSet(double[][] xDataSet){
-        double [][] yDataSet = new double[xDataSet.length][1];
-        for(int i = 0; i < yDataSet.length; i++){
+
+    public static double[][] initializeYDataSet(double[][] xDataSet) {
+        double[][] yDataSet = new double[xDataSet.length][1];
+        for (int i = 0; i < yDataSet.length; i++) {
             yDataSet[i][0] = Functions.computeRosenBrockOutVal(xDataSet[i]);
         }
         return yDataSet;
+    }
+
+    public static int[][] initializeSubsets(int dataSetSize, int k) {
+        int[] selectedIndexes = new int[dataSetSize];
+        for (int i = 0; i < selectedIndexes.length; i++) {
+            selectedIndexes[i] = -1;
+        }
+        Random rdm = new Random();
+        int randomIndex;
+        int selectedIndexesCounter = 0;
+        int[][] subsets = new int[k][dataSetSize / k];
+        for (int[] subset : subsets) {
+            for (int j = 0; j < subsets[0].length; j++) {
+                boolean counterAssigned = false;
+                while (!counterAssigned) {
+                    randomIndex = rdm.nextInt(dataSetSize);
+                    //System.out.println(randomIndex);
+                    if (!containsValue(selectedIndexes, randomIndex)) {
+                        subset[j] = randomIndex;
+                        selectedIndexes[selectedIndexesCounter] = randomIndex;
+                        selectedIndexesCounter++;
+                        counterAssigned = true;
+                    }
+                }
+            }
+        }
+        return subsets;
+    }
+
+    public static boolean containsValue(int[] array, int a) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == a) {
+                return true;
+            }
+        }
+        return false;
     }
 }
