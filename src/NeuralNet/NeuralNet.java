2754cf7e4049bd778146b8bba68f01c296f84255
angustomlinson <thekingsbondslave@gmail.com>
2015-10-14 12:10:41 -0600

    Update NeuralNet.java

--- src/NeuralNet/NeuralNet.java
+++ src/NeuralNet/NeuralNet.java
@@ -7,12 +7,13 @@ import NeuralNetHelper.*;
  * @author Angus Tomlinson
  */
 public final class NeuralNet {
-
-    //tunable parameters:
-    private final double eta = 0.3;
-    private final double upperBoundWeight = 1;
-    private final double upperBoundBiasWeight = 100;
-    private final double momentumParameter = 0.1;
+    private double upperBoundInitializationWeight = 1;
+    private double upperBoundInitializationBias = 1;
+    private double eta = 0.0001;
+    private double momentumParameter = 0.8;
+    
+    private int r = 1;
+    private int c = 2;
 
     private final boolean isHiddenLayerCountZero;
     private Matrix input;
@@ -27,15 +28,25 @@ public final class NeuralNet {
     private final Matrix[] lastWeightUpdates;
     private final Matrix[] lastBiasUpdates;
 
-    public NeuralNet(double[] input, double[] targetOutput, int[] hiddenLayers) {
+    // initialize the neural network
+    public NeuralNet(double[] input, double[] targetOutput, int[] hiddenLayers, double upperBoundInitializationWeight, double upperBoundInitializationBias, double eta,
+            double momentumParameter, int r, int c) {
+        this.eta = eta;
+        this.upperBoundInitializationWeight = upperBoundInitializationWeight;
+        this.upperBoundInitializationBias = upperBoundInitializationBias;
+        this.momentumParameter = momentumParameter;
+        this.r = r;
+        this.c = c;
+        
         this.input = new Matrix(input);
-        output = new Matrix(new double[targetOutput.length]);
+        this.output = new Matrix(new double[targetOutput.length]);
         this.targetOutput = new Matrix(targetOutput);
 
         isHiddenLayerCountZero = (hiddenLayers.length == 0);
 
         initializeWeights(hiddenLayers);
 
+        // the lastWeightUpdates and lastBiasUpdats are used for momentum
         lastWeightUpdates = new Matrix[weightMatrices.length];
         for (int i = 0; i < weightMatrices.length; i++) {
             lastWeightUpdates[i] = new Matrix(new double[weightMatrices[i].getRows()][weightMatrices[i].getColumns()]);
@@ -53,6 +64,7 @@ public final class NeuralNet {
         deltaZMatrices = new Matrix[hiddenLayers.length + 1];
     }
 
+    // randomly initialize the node and bias node weights
     public void initializeWeights(int[] hiddenLayers) {
         weightMatrices = new Matrix[hiddenLayers.length + 1];
         if (isHiddenLayerCountZero) {
@@ -66,7 +78,7 @@ public final class NeuralNet {
         }
 
         for (int i = 0; i < weightMatrices.length; i++) {
-            weightMatrices[i] = MatrixOperations.randomlyInitializeMatrix(weightMatrices[i], upperBoundWeight);
+            weightMatrices[i] = MatrixOperations.randomlyInitializeMatrix(weightMatrices[i], upperBoundInitializationWeight);
         }
 
         biasMatrices = new Matrix[hiddenLayers.length + 1];
@@ -80,10 +92,15 @@ public final class NeuralNet {
         }
 
         for (int i = 0; i < biasMatrices.length; i++) {
-            biasMatrices[i] = MatrixOperations.randomlyInitializeMatrix(biasMatrices[i], upperBoundBiasWeight);
+            biasMatrices[i] = MatrixOperations.randomlyInitializeMatrix(biasMatrices[i], upperBoundInitializationBias);
         }
     }
-
+    
+    // forward propagation:
+    // S(1) = W(1) * input
+    // Z(i) = f(S(i)) where f(x) is the sigmoid function
+    // S(i) = W(i) * Z(i - 1)
+    // F(i) = (f'(S(i)))^T
     public void forwardPropagation() {
         if (isHiddenLayerCountZero) {
             output = MatrixOperations.addMatrices(MatrixOperations.multiplyMatrixes(input, weightMatrices[0]), biasMatrices[0]);
@@ -101,6 +118,9 @@ public final class NeuralNet {
         }
     }
 
+    // back propagation:
+    // Delta(out) = (output - targetOutput)^T
+    // Delta(i) = hadamard product of F(i) and (W(i) * Delta(i + 1))
     public void backPropagation() {
         deltaZMatrices[deltaZMatrices.length - 1] = MatrixOperations.transpose(MatrixOperations.subtractMatrices(output, targetOutput));
 
@@ -112,6 +132,9 @@ public final class NeuralNet {
         updateBiases();
     }
 
+    // updates the weights:
+    // for the first weights: deltaW(1) = -eta * (Delta(1) * input)^T
+    // for the other weights: deltaW(i) = -eta * (Delta(i) * Z(i - 1))^T
     public void updateWeights() {
         Matrix deltaWeightUpdate = MatrixOperations.addMatrices(deltaWeight(deltaZMatrices[0], input), lastWeightUpdates[0]);
         lastWeightUpdates[0] = deltaWeightUpdate;
@@ -123,6 +146,8 @@ public final class NeuralNet {
         }
     }
 
+    // updates the biases:
+    // for biases: deltaBias(i) = -eta * (Delta(i))^T
     public void updateBiases() {
         Matrix deltaBiasUpdate;
         for (int i = 0; i < biasMatrices.length; i++) {
@@ -140,17 +165,13 @@ public final class NeuralNet {
                 MatrixOperations.transpose(MatrixOperations.multiplyMatrixes(deltaMatrix, zMatrix))));
         return deltaWeightMatrix;
     }
-
-    public Matrix getInputMatrix() {
-        return input;
-    }
     
     public void setInputMatrix(double[] inputMatrix){
         input = new Matrix(inputMatrix);
     }
-
-    public Matrix getOutputMatrix() {
-        return output;
+    
+    public void setOutputMatrix(double[] outputMatrix){
+        targetOutput = new Matrix(outputMatrix);
     }
 
     public Matrix getTargetOutputMatrix() {
@@ -161,8 +182,10 @@ public final class NeuralNet {
         targetOutput = new Matrix(targetOutputMatrix);
     }
 
+    // calculates the error: E = (1 / 2) * (output - targetOutput)^2
     public Matrix getError() {
-        Matrix errorMatrix = MatrixOperations.multiplyMatrixes(MatrixOperations.subtractMatrices(output, targetOutput), MatrixOperations.subtractMatrices(output, targetOutput));
+        Matrix errorMatrix = MatrixOperations.scalarMultiply(0.5, MatrixOperations.multiplyMatrixes(MatrixOperations.subtractMatrices(output, targetOutput), 
+                MatrixOperations.subtractMatrices(output, targetOutput)));
         return errorMatrix;
     }
 }
