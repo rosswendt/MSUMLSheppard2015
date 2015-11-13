b70932ec57c1edc01b960e21a9dafdc38a5ba10a
angustomlinson <thekingsbondslave@gmail.com>
2015-11-12 19:59:19 -0700

    Update DriverHelper.java
    Fixed cross-validation

--- src/Driver/DriverHelper.java
+++ src/Driver/DriverHelper.java
@@ -4,11 +4,53 @@ import java.util.Random;
 
 /**
  *
- * @author Ross Wendt
+ * @author Angus Tomlinson
  */
 public class DriverHelper {
-    
 
+    public static int[] randomizeIndexes(int dataSetSize) {
+        int[] randomizedIndexes = new int[dataSetSize];
+        for (int i = 0; i < randomizedIndexes.length; i++) {
+            randomizedIndexes[i] = -1;
+        }
+        Random rdm = new Random();
+        int randomIndex;
+        for (int i = 0; i < randomizedIndexes.length; i++) {
+            while (true) {
+                randomIndex = rdm.nextInt(dataSetSize);
+                if (!containsValue(randomizedIndexes, randomIndex)) {
+                    randomizedIndexes[i] = randomIndex;
+                    break;
+                }
+            }
+        }
+
+        return randomizedIndexes;
+    }
+    
+    public static double[][] randomizeDataSet(double[][] dataSet, int[] randomizedIndexes){
+        double[][] randomizedDataSet = new double[dataSet.length][dataSet[0].length];
+        for(int i = 0; i < randomizedDataSet.length; i++){
+            randomizedDataSet[i] = dataSet[randomizedIndexes[i]];
+        }
+        return randomizedDataSet;
+    }
+    
+    public static Subset[] generateSubsets(double[][] dataSet, int k){
+        Subset[] subsets = new Subset[k];
+        for(int i = 0; i < subsets.length; i++){
+            subsets[i] = new Subset(new double[dataSet.length / k][k]);
+        }
+        int dataSetCounter = 0;
+        for(int i = 0; i < subsets.length; i++){
+            for(int j = 0; j < (dataSet.length / k); j++){
+                subsets[i].getSubsetValues()[j] = dataSet[dataSetCounter++];
+            }
+        }
+        
+        return subsets;
+    }
+    
     public static boolean containsValue(int[] array, int a) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == a) {
@@ -18,32 +60,4 @@ public class DriverHelper {
         return false;
     }
 
-    public static int[][] initializeSubsets(int dataSetSize, int k) {
-        int[] selectedIndexes = new int[dataSetSize];
-        for (int i = 0; i < selectedIndexes.length; i++) {
-            selectedIndexes[i] = -1;
-        }
-        Random rdm = new Random();
-        int randomIndex;
-        int selectedIndexesCounter = 0;
-        int[][] subsets = new int[k][dataSetSize / k];
-        for (int[] subset : subsets) {
-            for (int j = 0; j < subsets[0].length; j++) {
-                boolean counterAssigned = false;
-                while (!counterAssigned) {
-                    randomIndex = rdm.nextInt(dataSetSize);
-                    //System.out.println(randomIndex);
-                    if (!containsValue(selectedIndexes, randomIndex)) {
-                        subset[j] = randomIndex;
-                        selectedIndexes[selectedIndexesCounter] = randomIndex;
-                        selectedIndexesCounter++;
-                        counterAssigned = true;
-                    }
-                }
-            }
-        }
-        return subsets;
-    }
-
-    
 }
